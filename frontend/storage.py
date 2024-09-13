33ae37364904efc998fdccce9ce3945d6fff53e2
bcx <femolak@gmail.com>
2024-09-13 11:35:09 +0200

    db update

--- frontend/storage.py
+++ frontend/storage.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Update the database URL to use PostgreSQL
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@db:5432/library"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgres:5432/library"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 # Remove the SQLite-specific connect_args
