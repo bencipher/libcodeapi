bb6fd30b9d7f75d7a8bb862ad1586a668a5f9d82
bcx <femolak@gmail.com>
2024-09-14 23:51:12 +0200

    alemnic setup

--- alembic/env.py
+++ alembic/env.py
@@ -11,14 +11,15 @@ config = context.config
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
-# if config.config_file_name is not None:
-#     fileConfig(config.config_file_name)
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
+from frontend.models import Base
+
+target_metadata = Base.metadata
+# target_metadata = None
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
